01512645071197aa2e7357551dfde8380360006c
gabrielpellizzon <gabrielpellizzon@gmail.com>
2024-09-29 09:00:58 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Macbook-Pro on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
